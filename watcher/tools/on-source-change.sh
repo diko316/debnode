5ec5ebbe3a137639ff3aed7c5cdd2d72d11dd779
Diko the tech slave <diko316@gmail.com>
2016-02-22 07:43:05 +0800

    fixed watcher to run first the app runner before observing it when starting the server

--- watcher/tools/on-source-change.sh
+++ watcher/tools/on-source-change.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 
+if ! which rsync > /dev/null; then
+    echo "rsync is not installed" >&2
+    exit 1
+fi
+
+
 rsync -rpDzh --exclude=".git*" --exclude="*node_modules/*" "${APP_SOURCE}" "${PROJECT_ROOT}/"
+
+exit $?
--- watcher/tools/watch-source.sh
+++ watcher/tools/watch-source.sh
@@ -1,16 +1,44 @@
 #!/bin/sh
 
 WATCH=$(which watch)
+PID=${LOG_FILES}/run.pid
+
+if [ -f "${PID}" ]; then
+    PROCESS_ID=$(cat ${PID})
+    if ! kill "${PROCESS_ID}"; then
+        kill -9 "${PROCESS_ID}" || exit 1
+    fi
+    rm "${PID}"
+fi
+
+PID=${LOG_FILES}/watch-source.pid
+
+if [ -f "${PID}" ]; then
+    PROCESS_ID=$(cat ${PID})
+    if ! kill "${PROCESS_ID}"; then
+        kill -9 "${PROCESS_ID}" || exit 1
+    fi
+    rm "${PID}"
+fi
 
 if [ -d "${APP_SOURCE}" ]; then
     echo "Running watch on source file change..."
     setsid ${WATCH} "${APP_TOOLS}/on-source-change.sh" "${APP_SOURCE}" > "${LOG_FILES}/watch.log" 2>&1 < /dev/null &
+    echo $! > "${PID}"
+    
 fi
+
 
 if [ -d "${PROJECT_ROOT}" ]; then
     echo "Running Watch on app runner..."
-    setsid ${WATCH} "${APP_TOOLS}/on-project-change.sh" "${PROJECT_ROOT}" > "${LOG_FILES}/change.log" 2>&1 < /dev/null &
+    #setsid ${WATCH} "${APP_TOOLS}/on-project-change.sh" "${PROJECT_ROOT}" > "${LOG_FILES}/change.log" 2>&1 < /dev/null &
+    
+    "${APP_TOOLS}/on-project-change.sh" &
+    ${WATCH} "${APP_TOOLS}/on-project-change.sh" "${PROJECT_ROOT}"
+    
+else
+    "${APP_TOOLS}/on-project-change.sh"
 fi
 
-"${APP_TOOLS}/on-project-change.sh"
+
 
