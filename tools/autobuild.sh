603ac5f3d0905a4b708395fa7794ab63c39bbb4f
Diko Tech Slave <diko316@gmail.com>
2016-03-26 19:30:45 +0800

    fixed autobuild to support package.json and bower.json installation for autobuild without preset.

--- tools/autobuild.sh
+++ tools/autobuild.sh
@@ -2,10 +2,18 @@
 
 CURRENT_CWD=$(dirname $(readlink -m $0))
 BUILDER="${CURRENT_CWD}/autobuild/${1}.sh"
+PACKAGE_JSON="/tmp/package.json"
+BOWER_JSON="/tmp/bower.json"
 
-
+# Call autobuild with preset
 if [ -f "${BUILDER}" ] && [ -x "${BUILDER}" ]; then
     ${BUILDER} || exit 1
+
+# Call autobuild without preset
+elif [ -f "${PACKAGE_JSON}" ] || [ -f "${BUILD_JSON}" ]; then
+    "$APP_TOOLS/installer/autobuild.sh" || exit 2
+    
+# Show Warning
 else
     echo "No builder used, or builder not found: $1"
     echo "* Please use one of the builders below:"
