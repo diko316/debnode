042807039d92af4829c33f23d9cc0e3399151fdb
Diko Tech Slave <diko316@gmail.com>
2016-03-29 22:43:23 +0800

    reapplied node_modules and bower_components cleanup

--- tools/installer/cleanup.sh
+++ tools/installer/cleanup.sh
@@ -12,7 +12,19 @@ rm -rf /usr/include \
         /usr/lib/node_modules/npm/man \
         /usr/lib/node_modules/npm/doc \
         /usr/lib/node_modules/npm/html \
-        /var/lib/apt/lists/* 
+        /var/lib/apt/lists/*
+
+rm -rf $(find "/usr/lib/node_modules" -name test -o -type d | grep '/test$')
+
+# cleanup $PROJECT_ROOT/node_modules
+if [ -d "${PROJECT_ROOT}/node_modules" ]; then
+    rm -rf $(find "${PROJECT_ROOT}/node_modules" -name test -o -type d | grep '/test$')
+fi
+
+# cleanup $PROJECT_ROOT/bower_components
+if [ -d "${PROJECT_ROOT}/bower_components" ]; then
+    rm -rf $(find "${PROJECT_ROOT}/bower_components" -name test -o -type d | grep '/test$')
+fi
 
 if [ -d "/root/.node-gyp" ]; then
     rm -rf "/root/.node-gyp"
