10fa0cb67b2b14f0ba13a22d6fbc52548ba6d010
Diko Tech Slave <diko316@gmail.com>
2016-03-29 22:16:04 +0800

    removed node_modules cleanup

--- tools/installer/cleanup.sh
+++ tools/installer/cleanup.sh
@@ -12,18 +12,7 @@ rm -rf /usr/include \
         /usr/lib/node_modules/npm/man \
         /usr/lib/node_modules/npm/doc \
         /usr/lib/node_modules/npm/html \
-        /var/lib/apt/lists/* \
-        $(find /usr/lib/node_modules -name test -o -type d)
-
-# cleanup $PROJECT_ROOT/node_modules
-if [ -d "${PROJECT_ROOT}/node_modules" ]; then
-    rm -rf $(find "${PROJECT_ROOT}/node_modules" -name test -o -type d)
-fi
-
-# cleanup $PROJECT_ROOT/bower_components
-if [ -d "${PROJECT_ROOT}/bower_components" ]; then
-    rm -rf $(find "${PROJECT_ROOT}/bower_components" -name test -o -type d)
-fi
+        /var/lib/apt/lists/* 
 
 if [ -d "/root/.node-gyp" ]; then
     rm -rf "/root/.node-gyp"
