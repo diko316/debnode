ac72a4176ff3171d447491e018ac8efec77337e3
Diko Tech Slave <diko316@gmail.com>
2016-04-05 02:23:50 +0800

    supress Error and Warning messages when cleaning up.

--- tools/installer/cleanup.sh
+++ tools/installer/cleanup.sh
@@ -4,7 +4,7 @@ apt-get clean || true
 apt-get autoclean || true
 apt-get autoremove -y || true
 
-dpkg --purge $(dpkg -l | grep "^rc" | tr -s ' ' | cut -d ' ' -f 2) || true
+dpkg --purge $(dpkg -l | grep "^rc" | tr -s ' ' | cut -d ' ' -f 2) 2>/dev/null || true
 
 rm -rf /usr/include \
         /usr/share/man \
@@ -14,36 +14,38 @@ rm -rf /usr/include \
         /usr/lib/node_modules/npm/man \
         /usr/lib/node_modules/npm/doc \
         /usr/lib/node_modules/npm/html \
-        /var/lib/apt/lists/*
+        /var/lib/apt/lists/* 2>/dev/null || true
 
-rm -rf $(find "/usr/lib/node_modules" -name test -o -type d | grep '/test$')
+rm -rf $(find "/usr/lib/node_modules" -name test -o -type d | grep '/test$') 2>/dev/null || true
 
 # cleanup $PROJECT_ROOT/node_modules
 if [ -d "${PROJECT_ROOT}/node_modules" ]; then
-    rm -rf $(find "${PROJECT_ROOT}/node_modules" -name test -o -type d | grep '/test$')
+    rm -rf $(find "${PROJECT_ROOT}/node_modules" -name test -o -type d | grep '/test$') 2>/dev/null || true
 fi
 
 # cleanup $PROJECT_ROOT/bower_components
 if [ -d "${PROJECT_ROOT}/bower_components" ]; then
-    rm -rf $(find "${PROJECT_ROOT}/bower_components" -name test -o -type d | grep '/test$')
+    rm -rf $(find "${PROJECT_ROOT}/bower_components" -name test -o -type d | grep '/test$') 2>/dev/null || true
 fi
 
 if [ -d "/root/.node-gyp" ]; then
-    rm -rf "/root/.node-gyp"
+    rm -rf "/root/.node-gyp" 2>/dev/null || true
 fi
 
 if [ -d "/root/.cache" ]; then
-    rm -rf "/root/.cache"
+    rm -rf "/root/.cache" 2>/dev/null || true
 fi
 
 if [ -d "/root/.config" ]; then
-    rm -rf "/root/.config"
+    rm -rf "/root/.config" 2>/dev/null || true
 fi
 
 if [ -d "/root/.local" ]; then
-    rm -rf "/root/.local"
+    rm -rf "/root/.local" 2>/dev/null || true
 fi
 
 if [ -d "/usr/share/doc" ]; then
-    rm -rf "/usr/share/doc"
+    rm -rf "/usr/share/doc" 2>/dev/null || true
 fi
+
+exit 0
